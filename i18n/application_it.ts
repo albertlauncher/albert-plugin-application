7d904e0078b833166749624552128f555aa5751a
Manuel Schneider <manuelschneid3r@gmail.com>
2026-08-08 16:39:40 +0200

    feat: Add italian  translations

---
+++ i18n/application_it.ts
@@ -0,0 +1,63 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="it_IT">
+<context>
+    <name>Plugin</name>
+    <message>
+        <source>Cache location</source>
+        <translation>Percorso della cache</translation>
+    </message>
+    <message>
+        <source>Albert cache location</source>
+        <translation>Percorso della cache di Albert</translation>
+    </message>
+    <message>
+        <source>Config location</source>
+        <translation>Percorso della configurazione</translation>
+    </message>
+    <message>
+        <source>Albert config location</source>
+        <translation>Percorso della configurazione di Albert</translation>
+    </message>
+    <message>
+        <source>Data location</source>
+        <translation>Percorso dei dati</translation>
+    </message>
+    <message>
+        <source>Albert data location</source>
+        <translation>Percorso dei dati di Albert</translation>
+    </message>
+    <message>
+        <source>Open</source>
+        <translation>Apri</translation>
+    </message>
+    <message>
+        <source>Open in terminal</source>
+        <translation>Apri nel terminale</translation>
+    </message>
+    <message>
+        <source>Quit</source>
+        <translation>Esci</translation>
+    </message>
+    <message>
+        <source>Quit Albert</source>
+        <translation>Esci da Albert</translation>
+    </message>
+    <message>
+        <source>Restart</source>
+        <translation>Riavvia</translation>
+    </message>
+    <message>
+        <source>Restart Albert</source>
+        <translation>Riavvia Albert</translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation>Impostazioni</translation>
+    </message>
+    <message>
+        <source>Albert settings</source>
+        <translation>Impostazioni di Albert</translation>
+    </message>
+</context>
+</TS>
